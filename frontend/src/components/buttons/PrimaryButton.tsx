bb89db9df5033a7d4898abc8bc4b114d088c37f7
Sam Youatt <sdyouatt@gmail.com>
2025-04-26 16:46:45 +0100

    merge classNames

--- frontend/src/components/buttons/PrimaryButton.tsx
+++ frontend/src/components/buttons/PrimaryButton.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface PrimaryButtonProps {
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
     disabled?: boolean;
     children: React.ReactNode;
     type?: any;
+    className?: string;
 }
 
 export const PrimaryButton: FC<PrimaryButtonProps> = ({
@@ -12,16 +14,22 @@ export const PrimaryButton: FC<PrimaryButtonProps> = ({
     disabled = false,
     children,
     type,
+    className = '',
 }) => {
     const enabledStyles =
         'w-full rounded bg-pink-400 px-4 py-2 font-bold text-white hover:bg-pink-500';
     const disabledStyles =
         'w-full rounded bg-gray-300 px-4 py-2 font-bold text-gray-400';
+    const styles = twMerge(
+        disabled ? disabledStyles : enabledStyles,
+        className
+    );
+
     return (
         <button
             onClick={onClick}
             disabled={disabled}
-            className={disabled ? disabledStyles : enabledStyles}
+            className={styles}
             type={type}
         >
             {children}
