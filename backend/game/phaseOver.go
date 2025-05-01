6daa599d308ed436ad80b91cea20a6f71a46b425
James Potter <jamesrpotter1@gmail.com>
2025-05-01 08:40:17 +0100

    implement game over handler

--- backend/game/phaseOver.go
+++ backend/game/phaseOver.go
@@ -1,6 +1,10 @@
 package game
 
-import "backend/messages"
+import (
+	"backend/messages"
+	"encoding/json"
+	"log"
+)
 
 type GameOverHandler struct{}
 
@@ -9,12 +13,24 @@ func (p *GameOverHandler) Phase() GamePhase {
 }
 
 func (p *GameOverHandler) StartPhase(gs *GameState) {
-	// todo
-	return
+	log.Println("GameState: Entering GameOver phase.")
+	gs.IsActive = false
+
+	finalScoresPayload := messages.GameFinishedPayload{
+		Players: gs.getPlayerInfoList(),
+	}
+
+	gameOverMsg := messages.Message{
+		Type:    messages.GameFinishedResponse,
+		Payload: json.RawMessage(messages.MustMarshal(finalScoresPayload)),
+	}
+
+	log.Printf("GameState: Broadcasting GameFinished message with %d players.", len(finalScoresPayload.Players))
+	go gs.Broadcaster.Broadcast(gameOverMsg)
 }
 
 func (p *GameOverHandler) HandleMessage(gs *GameState, player *Player, msg messages.Message) GamePhaseHandler {
-	// todo
+	log.Printf("GameState: Ignoring message type %s from player %s in GameOver phase.", msg.Type, player.Name)
 	return p
 }
 
