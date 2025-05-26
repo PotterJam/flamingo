db99dc5390986f6e3ad6a360f6b3529f2f324f38
Sam Youatt <sdyouatt@gmail.com>
2025-05-26 14:33:43 +0100

    fun outline button

--- frontend/src/components/buttons/OutlineButton.tsx
+++ frontend/src/components/buttons/OutlineButton.tsx
@@ -36,3 +36,21 @@ export const OutlineButton: FC<OutlineButtonProps> = ({
         </button>
     );
 };
+
+
+export const FunOutlineButton: FC<OutlineButtonProps> = ({
+    onClick,
+    disabled = false,
+    children,
+    className = '',
+}) => {
+    return (
+        <button
+            disabled={disabled}
+            className={`group relative inline-flex items-center justify-center overflow-hidden rounded-md bg-white px-4 py-2 font-bold text-pink-400 border-2 disabled:border-gray-300 disabled:text-gray-400 border-pink-400 [box-shadow:0px_4px_1px_#f472b6] transition-all active:translate-y-[4px] active:shadow-none disabled:cursor-not-allowed disabled:bg-gray-300 disabled:shadow-none disabled:active:translate-y-0 ${className}`}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+};
