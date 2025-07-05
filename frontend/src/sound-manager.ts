39fc6e222efd6caedbf1e4807655c86388379c7a
Sam Youatt <sdyouatt@gmail.com>
2025-07-05 19:41:21 +0100

    create sound manager

---
+++ frontend/src/sound-manager.ts
@@ -0,0 +1,28 @@
+import correctTone from './assets/sounds/correct-tone.wav';
+
+export const SOUNDS = {
+    correct: correctTone,
+} as const;
+
+export type SoundName = keyof typeof SOUNDS;
+
+function createSoundManager() {
+    const sounds: Record<string, HTMLAudioElement> = {};
+
+    const loadSounds = () => {
+        for (const [name, url] of Object.entries(SOUNDS)) {
+            sounds[name] = new Audio(url);
+            sounds[name].preload = 'auto';
+        }
+    };
+
+    const playSound = (name: SoundName) => {
+        const sound = sounds[name];
+        sound.currentTime = 0;
+        sound.play();
+    };
+
+    return { loadSounds, playSound };
+}
+
+export const soundManager = createSoundManager();
