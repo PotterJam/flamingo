3abae8968cecd7779d5210c47285abf8ff174cbf
Sam Youatt <sdyouatt@gmail.com>
2025-07-12 15:15:06 +0100

    add some null states and stuff

--- frontend/src/components/WordDisplay.tsx
+++ frontend/src/components/WordDisplay.tsx
@@ -1,3 +1,5 @@
+import { Show } from 'solid-js';
+
 interface WordDisplayProps {
     word?: string;
     wordOutline?: string[];
@@ -12,9 +14,16 @@ function WordDisplay(props: WordDisplayProps) {
             .map((char) => char || '_')
             .join('');
 
+    if (!word() && !wordOutline().length) {
+        return null;
+    }
+
     return (
-        <div class="font-retro flex-1 translate-y-0.75 text-5xl text-amber-400">
-            <p>{wordDisplay()}</p>
+        <div class="font-retro flex flex-1 flex-row justify-center gap-12 text-5xl text-amber-400">
+            <p class="translate-y-0.75">{wordDisplay()}</p>
+            <Show when={!word()}>
+                <p class="translate-y-0.75">({wordOutline().length})</p>
+            </Show>
         </div>
     );
 }
