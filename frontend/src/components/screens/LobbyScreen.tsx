3d0665cdd57b152a8795971159257f72b924b38e
Sam Youatt <sdyouatt@gmail.com>
2025-07-15 10:20:57 +0100

    finish tidying up lobby screen for host

--- frontend/src/components/screens/LobbyScreen.tsx
+++ frontend/src/components/screens/LobbyScreen.tsx
@@ -1,8 +1,5 @@
-import { For, Show } from 'solid-js';
+import { For } from 'solid-js';
 import { actions, store } from '../../store';
-import { OutlineButton } from '../buttons/OutlineButton';
-import { PrimaryButton } from '../buttons/PrimaryButton';
-import { CANVAS_HEIGHT } from '../Game';
 import { MIN_PLAYERS } from '../../App';
 import { Card } from '../ui/card';
 import { Button } from '../ui/button';
@@ -14,6 +11,7 @@ import {
     SliderTrack,
     SliderValueLabel,
 } from '../ui/slider';
+import { Label } from '../ui/label';
 
 export const LobbyScreen = () => {
     const isHost = () =>
@@ -39,7 +37,7 @@ export const LobbyScreen = () => {
     return (
         <div class="flex h-full w-full flex-grow items-center justify-center">
             <Card class="flex h-3/5 w-full max-w-2xl flex-row gap-0 bg-white p-0">
-                <div class="flex flex-1 flex-col gap-4 border-r-2 border-black p-4">
+                <div class="flex flex-1 flex-col gap-4 border-r-2 border-blue-950 p-4">
                     <h2 class="text-xl font-bold">Players</h2>
                     <div class="overflowy-auto h-full">
                         <ul class="space-y-2">
@@ -52,41 +50,45 @@ export const LobbyScreen = () => {
                     </div>
                 </div>
                 <div class="flex h-full w-full flex-3 flex-col gap-4 p-4">
-                    <div class="flex flex-row items-center justify-between">
-                        <p class="text-l font-bold text-blue-400">
-                            {store.roomId}
-                        </p>
-                        <OutlineButton class="w-20" onClick={copyRoomName}>
-                            Copy
-                        </OutlineButton>
-                    </div>
-
-                        <Slider
-                            minValue={1}
-                            maxValue={5}
-                            defaultValue={[3]}
-                            value={[store.roundCount]}
-                            onChange={(x) => actions.setRoundCount(x[0])}
-                            class="space-y-3"
-                        >
-                            <div class="flex w-full justify-between">
-                                <SliderLabel>Rounds</SliderLabel>
-                                <SliderValueLabel />
-                            </div>
-                            <SliderTrack>
-                                <SliderFill />
-                                <SliderThumb />
-                            </SliderTrack>
-                        </Slider>
-
-                    <Button
-                        variant="default"
-                        class="mt-auto"
-                        onClick={handleStartGame}
-                        disabled={!canHostStartGame()}
+                    <Slider
+                        minValue={1}
+                        maxValue={5}
+                        defaultValue={[3]}
+                        value={[store.roundCount]}
+                        onChange={(x) => actions.setRoundCount(x[0])}
+                        class="space-y-3"
                     >
-                        Start Game
-                    </Button>
+                        <div class="flex w-full justify-between">
+                            <SliderLabel>Rounds</SliderLabel>
+                            <SliderValueLabel />
+                        </div>
+                        <SliderTrack>
+                            <SliderFill />
+                            <SliderThumb />
+                        </SliderTrack>
+                    </Slider>
+
+                    <div class="mt-auto flex w-full flex-col gap-4">
+                        <div>
+                            <Label>Room name</Label>
+                            <div class="flex flex-row items-center justify-between">
+                                <p class="text-l font-bold">{store.roomId}</p>
+                                <Button
+                                    variant="outline-no-shadow"
+                                    onClick={copyRoomName}
+                                >
+                                    Copy
+                                </Button>
+                            </div>
+                        </div>
+                        <Button
+                            variant="default"
+                            onClick={handleStartGame}
+                            disabled={!canHostStartGame()}
+                        >
+                            Start Game
+                        </Button>
+                    </div>
                 </div>
             </Card>
         </div>
