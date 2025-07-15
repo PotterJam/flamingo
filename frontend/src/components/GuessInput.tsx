2982818e459d0292933131e5c1adca17da7ca747
Sam Youatt <sdyouatt@gmail.com>
2025-07-15 19:19:59 +0100

    move over guess input

--- frontend/src/components/GuessInput.tsx
+++ frontend/src/components/GuessInput.tsx
@@ -1,5 +1,6 @@
 import { createSignal, Component } from 'solid-js';
 import { autofocus } from '@solid-primitives/autofocus';
+import { TextField, TextFieldInput, TextFieldLabel } from './ui/text-field';
 autofocus;
 
 interface GuessInputProps {
@@ -19,27 +20,26 @@ const GuessInput: Component<GuessInputProps> = ({ onGuess }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div class="relative">
-                <input
+        <form onSubmit={handleSubmit} class="p-4">
+            <TextField
+                value={currentGuess()}
+                onChange={(e) => setCurrentGuess(e)}
+                onSubmit={handleSubmit}
+                class="flex flex-col gap-2"
+            >
+                <TextFieldLabel>
+                    <div class="flex w-full flex-row justify-between px-1">
+                        <p>Guess</p>
+                        <p>{currentGuess().length}</p>
+                    </div>
+                </TextFieldLabel>
+                <TextFieldInput
                     type="text"
-                    value={currentGuess()}
-                    onInput={(e) =>
-                        setCurrentGuess((e.target as HTMLInputElement).value)
-                    }
-                    placeholder=""
-                    maxLength={50}
-                    class="w-full rounded border border-gray-300 px-2 py-2 pr-8 text-sm transition duration-150 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                    aria-label="Type message or guess"
+                    maxlength={12}
                     use:autofocus
                     autofocus
                 />
-                {currentGuess().length > 0 && (
-                    <div class="pointer-events-none absolute top-1/2 right-2 -translate-y-1/2 text-xs text-gray-500">
-                        {currentGuess().length}
-                    </div>
-                )}
-            </div>
+            </TextField>
         </form>
     );
 };
