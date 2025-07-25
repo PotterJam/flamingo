29583567b74890e5d27404d959b7b8b61d29cf5e
Sam Youatt <sdyouatt@gmail.com>
2025-07-25 20:02:47 +0100

    show the word letter count to guessers

--- frontend/src/components/WordDisplay.tsx
+++ frontend/src/components/WordDisplay.tsx
@@ -1,8 +1,13 @@
+import { Show } from 'solid-js';
 import { store } from '../store';
 
 function WordDisplay() {
     const word = () => store.gameState.word;
     const wordOutline = () => store.gameState.wordOutline ?? [];
+    const currentDrawerId = () => store.gameState.currentDrawerId;
+    const localPlayerId = () => store.gameState.localPlayerId;
+
+    const isLocalPlayerDrawer = () => localPlayerId() === currentDrawerId();
 
     const wordDisplay = () =>
         word() ||
@@ -10,14 +15,24 @@ function WordDisplay() {
             .map((char) => (char ? char.toUpperCase() : '_'))
             .join('');
 
+    const letterCount = () => wordOutline().length;
+
+    const showLetterCount = () =>
+        !word() && !isLocalPlayerDrawer() && wordOutline().length > 0;
+
     if (!word() && !wordOutline().length) {
         return null;
     }
 
     return (
-        <p class="flex flex-1 flex-row justify-center text-3xl font-black tracking-widest text-white">
-            {word() ? word()?.toUpperCase() : wordDisplay()}
-        </p>
+        <div class="flex flex-row items-center justify-center gap-4">
+            <p class="text-3xl font-black tracking-widest text-white">
+                {word() ? word()?.toUpperCase() : wordDisplay()}
+            </p>
+            <Show when={showLetterCount()}>
+                <p class="text-md text-white">{letterCount()}</p>
+            </Show>
+        </div>
     );
 }
 
