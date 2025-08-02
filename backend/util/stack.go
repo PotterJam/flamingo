f138a5aad731a6a2ff15cffb48c61f0fe38f2298
Sam Youatt <sdyouatt@gmail.com>
2025-08-02 12:00:17 +0100

    add a generic stack data structure

---
+++ backend/util/stack.go
@@ -0,0 +1,30 @@
+package util
+
+type Stack[T any] struct {
+	items []T
+}
+
+func New[T any]() *Stack[T] {
+	return &Stack[T]{
+		items: make([]T, 0),
+	}
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.items)
+}
+
+// Pop is unchecked so will throw if its empty
+func (s *Stack[T]) Pop() T {
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item
+}
+
+func (s *Stack[T]) Push(i T) {
+	s.items = append(s.items, i)
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.items) == 0
+}
