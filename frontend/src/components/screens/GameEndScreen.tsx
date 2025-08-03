72cd717b0602e592f0a40743e04a67464cbb5d6c
Sam Youatt <sdyouatt@gmail.com>
2025-08-03 19:53:23 +0100

    sketch out ui for going through drawings

--- frontend/src/components/screens/GameEndScreen.tsx
+++ frontend/src/components/screens/GameEndScreen.tsx
@@ -1,6 +1,23 @@
-import { For } from 'solid-js';
+import { createSignal, For } from 'solid-js';
 import { store } from '../../store';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '../ui/select';
+import { Player } from '~/messages';
+import {
+    Slider,
+    SliderFill,
+    SliderLabel,
+    SliderThumb,
+    SliderTrack,
+    SliderValueLabel,
+} from '../ui/slider';
+import { FiArrowLeft, FiArrowRight } from 'solid-icons/fi';
 
 const getMedal = (index: number): string => {
     switch (index) {
@@ -16,13 +33,19 @@ const getMedal = (index: number): string => {
 };
 
 export const GameEndScreen = () => {
-    const finalPlayers = [...store.gameState.players].sort((a, b) => b.score - a.score);
+    const [selectedPlayer, setSelectedPlayer] = createSignal<Player | null>(
+        null
+    );
+
+    const finalPlayers = [...store.gameState.players].sort(
+        (a, b) => b.score - a.score
+    );
 
     return (
-        <div class="fixed inset-0 z-50 flex items-center justify-center">
-            <Card class="w-sm gap-2 bg-white">
+        <div class="flex h-full w-full items-center justify-center gap-10">
+            <Card class="w-xs gap-2 bg-white">
                 <CardHeader>
-                    <CardTitle class="text-2xl">Game complete</CardTitle>
+                    <CardTitle class="text-2xl">Scores</CardTitle>
                 </CardHeader>
                 <CardContent>
                     <ul>
@@ -44,6 +67,56 @@ export const GameEndScreen = () => {
                             )}
                         </For>
                     </ul>
+                </CardContent>
+            </Card>
+            <Card class="gap-6 bg-white">
+                <CardHeader class="flex flex-row items-center gap-4">
+                    <CardTitle class="text-xl">Drawings from</CardTitle>
+                    <Select
+                        value={selectedPlayer()}
+                        onChange={setSelectedPlayer}
+                        options={finalPlayers}
+                        placeholder="Select a player..."
+                        optionValue="id"
+                        optionTextValue="name"
+                        itemComponent={(props) => (
+                            <SelectItem item={props.item}>
+                                {props.item.rawValue.name}
+                            </SelectItem>
+                        )}
+                    >
+                        <SelectTrigger class="mt-1 w-64">
+                            <SelectValue<Player>>
+                                {(state) => state.selectedOption().name}
+                            </SelectValue>
+                        </SelectTrigger>
+                        <SelectContent />
+                    </Select>
+                </CardHeader>
+                <CardContent>
+                    <div class="flex flex-row items-center">
+                        <button>
+                            <FiArrowLeft class="size-6"/>
+                        </button>
+                        <p class="w-full text-center text-2xl font-semibold">
+                            Magnet
+                        </p>
+                        <button>
+                            <FiArrowRight  class="size-6"/>
+                        </button>
+                    </div>
+                    <div class="mt-2 h-[500px] w-[700px] border-2" />
+                    <Slider
+                        minValue={1}
+                        maxValue={5}
+                        defaultValue={[3]}
+                        class="mt-6 space-y-3"
+                    >
+                        <SliderTrack>
+                            <SliderFill />
+                            <SliderThumb />
+                        </SliderTrack>
+                    </Slider>
                 </CardContent>
             </Card>
         </div>
