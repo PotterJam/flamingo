92ab727ffe2e9d6269a29aebff819995f2f37c59
Sam Youatt <sdyouatt@gmail.com>
2025-08-09 09:37:43 +0100

    Revert "use strokes for lines instead of raw pixels"
    This reverts commit e282f49f83ee2566451b9cf669b6a1f3d466ba8a.

--- frontend/src/hooks/useCanvas.ts
+++ frontend/src/hooks/useCanvas.ts
@@ -70,16 +70,21 @@ export const useCanvas = ({ canvasCtx }: UseCanvasProps) => {
         thickness: number,
         hexColour: string
     ) => {
-        canvasCtx.imageSmoothingEnabled = false;
-        canvasCtx.strokeStyle = hexColour;
-        canvasCtx.lineWidth = thickness;
-        canvasCtx.lineCap = 'round';
-        canvasCtx.lineJoin = 'miter';
+        if (startX === endX && startY === endY)
+            drawAtCoord(startX, startY, thickness, hexColour);
 
-        canvasCtx.beginPath();
-        canvasCtx.moveTo(Math.round(startX), Math.round(startY));
-        canvasCtx.lineTo(Math.round(endX), Math.round(endY));
-        canvasCtx.stroke();
+        const dx = endX - startX;
+        const dy = endY - startY;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const steps = Math.max(Math.ceil(distance), 1);
+
+        for (let i = 0; i <= steps; i++) {
+            const t = i / steps; // Linear interpolation
+            const x = Math.round(startX + dx * t);
+            const y = Math.round(startY + dy * t);
+
+            drawAtCoord(x, y, thickness, hexColour);
+        }
     };
 
     const fill = (rootX: number, rootY: number, colour: string) => {
