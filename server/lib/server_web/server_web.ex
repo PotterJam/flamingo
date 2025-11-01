bbbe9d947b014dedcfc6b7cd3fa1a2cdc011cbe1
Sam Youatt <samyouatt@Sams-MacBook-Pro.local>
2025-11-01 18:55:52 +0000

    create base controller macros
    separate ones for controllers (returning json) and html_controllers
    (returning html). Not 100% sure this is the _best_ way of doing this.
    Normally these would get generated by phx

---
+++ server/lib/server_web/server_web.ex
@@ -0,0 +1,19 @@
+defmodule ServerWeb do
+  def controller do
+    quote do
+      use Phoenix.Controller, formats: [:json]
+      import Plug.Conn
+    end
+  end
+
+  def html_controller do
+    quote do
+      use Phoenix.Controller, formats: [:html]
+      import Plug.Conn
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
