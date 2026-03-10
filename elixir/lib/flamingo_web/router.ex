28c588aae8f4aea2ca207bc7fd8eafbd2b0439cc
Sam Youatt <31775205+SamYouatt@users.noreply.github.com>
2026-03-10 20:32:13 +0000

    scaffold phoenix liveview app with conductor setup (#111)
    * plan
    
    * scaffold phoenix liveview app
    
    * copy game assets from existing project
    
    * add conductor setup and run scripts

---
+++ elixir/lib/flamingo_web/router.ex
@@ -0,0 +1,27 @@
+defmodule FlamingoWeb.Router do
+  use FlamingoWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {FlamingoWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", FlamingoWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", FlamingoWeb do
+  #   pipe_through :api
+  # end
+end
