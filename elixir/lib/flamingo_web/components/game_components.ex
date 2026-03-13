84325e948dd2cfacafe79554c538084e60a6d454
Sam Youatt <sdyouatt@gmail.com>
2026-03-13 23:06:23 +0000

    refine drawing toolbar styling and button component
    move button_group to core_components as a reusable primitive. add size attr to button component with sm variant. shrink undo/redo/clear buttons and fix pink background on active button group items by removing competing bg-white classes. change fill tool icon to arrows-pointing-out.

--- elixir/lib/flamingo_web/components/game_components.ex
+++ elixir/lib/flamingo_web/components/game_components.ex
@@ -1,7 +1,7 @@
 defmodule FlamingoWeb.GameComponents do
   use Phoenix.Component
 
-  import FlamingoWeb.CoreComponents, only: [icon: 1]
+  import FlamingoWeb.CoreComponents, only: [icon: 1, button_group: 1]
 
   attr :class, :any, default: nil
   slot :inner_block, required: true
@@ -84,20 +84,6 @@ defmodule FlamingoWeb.GameComponents do
     """
   end
 
-  attr :class, :any, default: nil
-  slot :inner_block, required: true
-
-  def button_group(assigns) do
-    ~H"""
-    <div class={[
-      "flex border-2 border-border bg-white shadow-shadow",
-      @class
-    ]}>
-      {render_slot(@inner_block)}
-    </div>
-    """
-  end
-
   attr :palette, :list, required: true
 
   def drawing_toolbar(assigns) do
@@ -117,7 +103,7 @@ defmodule FlamingoWeb.GameComponents do
         <button
           :for={size <- [6, 9, 15]}
           data-size={size}
-          class="flex h-10 w-10 cursor-pointer items-center justify-center border-r-2 border-border bg-white last:border-r-0"
+          class="flex h-10 w-10 cursor-pointer items-center justify-center border-r-2 border-border last:border-r-0"
         >
           <div
             class="rounded-full bg-black"
@@ -130,36 +116,36 @@ defmodule FlamingoWeb.GameComponents do
       <.button_group>
         <button
           data-tool="pen"
-          class="flex h-10 w-10 cursor-pointer items-center justify-center border-r-2 border-border bg-white"
+          class="flex h-10 w-10 cursor-pointer items-center justify-center border-r-2 border-border"
         >
           <.icon name="hero-paint-brush" class="h-5 w-5" />
         </button>
         <button
           data-tool="fill"
-          class="flex h-10 w-10 cursor-pointer items-center justify-center bg-white"
+          class="flex h-10 w-10 cursor-pointer items-center justify-center"
         >
-          <.icon name="hero-arrows-pointing-in" class="h-5 w-5" />
+          <.icon name="hero-arrows-pointing-out" class="h-5 w-5" />
         </button>
       </.button_group>
 
       <div class="flex gap-1">
         <button
           data-action="undo"
-          class="flex h-10 w-10 cursor-pointer items-center justify-center hover:bg-pink-100"
+          class="flex h-8 w-8 cursor-pointer items-center justify-center hover:bg-pink-100"
         >
-          <.icon name="hero-arrow-uturn-left" class="h-5 w-5" />
+          <.icon name="hero-arrow-uturn-left" class="h-4 w-4" />
         </button>
         <button
           data-action="redo"
-          class="flex h-10 w-10 cursor-pointer items-center justify-center hover:bg-pink-100"
+          class="flex h-8 w-8 cursor-pointer items-center justify-center hover:bg-pink-100"
         >
-          <.icon name="hero-arrow-uturn-right" class="h-5 w-5" />
+          <.icon name="hero-arrow-uturn-right" class="h-4 w-4" />
         </button>
         <button
           data-action="clear"
-          class="flex h-10 w-10 cursor-pointer items-center justify-center hover:bg-pink-100"
+          class="flex h-8 w-8 cursor-pointer items-center justify-center hover:bg-pink-100"
         >
-          <.icon name="hero-trash" class="h-5 w-5" />
+          <.icon name="hero-trash" class="h-4 w-4" />
         </button>
       </div>
     </div>
