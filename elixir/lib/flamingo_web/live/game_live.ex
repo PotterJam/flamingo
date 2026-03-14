1a2a9fa2b6988e954cd76b10122f08b8a4432428
Sam Youatt <sdyouatt@gmail.com>
2026-03-14 20:07:22 +0000

    remove timer from turn reveal screen

--- elixir/lib/flamingo_web/live/game_live.ex
+++ elixir/lib/flamingo_web/live/game_live.ex
@@ -268,14 +268,6 @@ defmodule FlamingoWeb.GameLive do
                   <.box class="flex w-[704px] shrink-0 flex-col items-center justify-center gap-6 bg-white">
                     <p class="font-timer text-2xl font-black text-gray-500">The word was</p>
                     <p class="font-timer text-5xl font-black text-pink-400">{@word}</p>
-                    <.starburst_timer
-                      position_class=""
-                      size_class="h-20 w-20"
-                      text_class="text-xl"
-                      timer_id="turn-reveal-timer"
-                      timer_hook="FlamingoWeb.GameLive.Timer"
-                      end_time={@turn_end_time && DateTime.to_iso8601(@turn_end_time)}
-                    />
                   </.box>
                 <% true -> %>
                   <div class="flex w-[704px] shrink-0 flex-col gap-4">
