ac40ad82dda6054874beeb68a33c462358b1f8aa
Sam Youatt <31775205+SamYouatt@users.noreply.github.com>
2026-05-26 19:51:09 +0100

    Remove go version (#149)
    * delete go
    
    * simplify docs

--- frontend/src/components/TimerDisplay.tsx
+++
@@ -1,9 +0,0 @@
-import { useTimer } from '../hooks/useTimer';
-
-function TimerDisplay(props: { endTime: number }) {
-    const remainingSeconds = useTimer(() => props.endTime);
-
-    return <p class="text-xl font-bold text-white">{remainingSeconds()}</p>;
-}
-
-export default TimerDisplay;
