101c41da2b55bae2b055ce35a5819f57945a78a2
Sam Youatt <sdyouatt@gmail.com>
2026-05-26 19:20:49 +0100

    delete go

--- frontend/src/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-export const Logo = () => {
-    return (
-        <h1 class="font-retro-display text-2xl font-bold text-pink-400">
-            flamin<span class="text-sky-400">go</span>
-        </h1>
-    );
-};
